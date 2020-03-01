829ac00a0e2b0252779d26eb4b9747d3727cced9
eddiebrett <eddiebrett@hotmail.co.uk>
2020-03-01 11:18:10 +0000

    identatin error

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -117,7 +117,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
- AUTHENTICATION_BACKENDS = [
+AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
     'accounts.backends.EmailAuth'
 ]
